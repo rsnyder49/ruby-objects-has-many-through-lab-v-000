0fa478a2c1ca884d638a816305b33e49811a8903
Robert Snyder <r.snyder49@gmail.com>
2018-08-30 23:04:23 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,3 +1,16 @@
 class Genre 
   @@all  []
+  
+  def initialize(name, artist)
+    @name = name 
+    @artist = artist 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_song(song_name, artist)
+  end 
+  
 end 
