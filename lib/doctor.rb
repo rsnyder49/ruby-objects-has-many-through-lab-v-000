73a3f4f1ec8f537b596d05c6268c79b037bc503d
Robert Snyder <r.snyder49@gmail.com>
2018-08-31 00:32:03 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,13 @@
+class Doctor 
+  attr_accessor :name
+  
+  @@all = [] 
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def new_appointment(date, patient)
+    Appointment.new()
+end 
